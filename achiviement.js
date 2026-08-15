16ef5fcf449c075dbfc51f13af6865fed32132e4
Pablo Vinícius Alves Pisolato <pblalves29@gmail.com>
2026-08-14 22:46:24 -0300

    Atualizando projeto

--- achiviement.js
+++ achiviement.js
@@ -1,1 +1,40 @@
-console.log("Achei");
+let alunoNotas = [4, 7, 8, 7, 9]; 
+ 
+let somaArray = 0;
+function somaMedia(){
+    for (let i = 0; i < alunoNotas.length; i++){
+        console.log(alunoNotas[i]);
+        somaArray += alunoNotas[i];
+        
+    }
+}
+
+let mediaTotal = 0;
+
+function calculoMedia(){
+    mediaTotal = (somaArray / alunoNotas.length);
+    if (mediaTotal >= 7){
+        console.log(`Sua média final é: ${mediaTotal} - Aprovado`);
+    }
+    else {
+        console.log(`Sua média final é: ${mediaTotal} - Reprovado`);
+    }
+}
+
+somaMedia();
+console.log(somaArray);
+calculoMedia();
+
+let contador = 0;
+let mediaClasse = 7;
+
+function maiorMenor(){
+    for(let i = 0; i < alunoNotas.length; i++){
+        if (alunoNotas[i] >= mediaClasse){
+            contador ++;
+        } 
+    }
+}
+
+maiorMenor();
+console.log(`O aluno ficou com ${contador} notas acima da média.`);
